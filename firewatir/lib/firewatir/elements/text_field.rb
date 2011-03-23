f02ebda3cbe72f425ec7eea1d86196888657a1cf
Jason Kroll <jason@insidejobs.com>
2011-03-23 11:37:50 -0700

    [change] faster key press

--- firewatir/lib/firewatir/elements/text_field.rb
+++ firewatir/lib/firewatir/elements/text_field.rb
@@ -185,27 +185,11 @@ module FireWatir
     # Input:
     #   - value - The string to enter into the text field
     #
-    def doKeyPress( value )
-      begin
-        max = maxlength
-        if (max > 0 && value.length > max)
-          original_value = value
-          value = original_value[0...max]
-          element.log " Supplied string is #{suppliedValue.length} chars, which exceeds the max length (#{max}) of the field. Using value: #{value}"
-        end
-      rescue
-        # probably a text area - so it doesnt have a max Length
-      end
-      for i in 0..value.length-1
-        #sleep element.typingspeed   # typing speed
-        c = value[i,1]
-        #element.log  " adding c.chr " + c  #.chr.to_s
-        @o.value = "#{(@o.value.to_s + c)}"   #c.chr
-        @o.fireEvent("onKeyDown")
-        @o.fireEvent("onKeyPress")
-        @o.fireEvent("onKeyUp")
-      end
 
+    # Faster key press, from http://khughes88.wordpress.com/2009/11/02/firewatir-slow-on-ubuntu-one-answer/
+    def doKeyPress( value )
+      @o.value=value
+      fire_key_events
     end
     private :doKeyPress
 
